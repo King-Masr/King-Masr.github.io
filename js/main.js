afdf252669037f4fd8cb72a32496b9b5388fdf5f
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-01 09:23:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-  document.querySelector("#facebook").herf = "https://www.facebook.com/KingIMasr";
-  document.getElementById("twitter").herf = "https://www.twitter.com/KingIMasr";
-  document.getElementById("linkedin").herf = "https://www.linkedin.com/in/king-masr/";
-  document.getElementById("telegram").herf = "https://t.me/King_Masr";
 };
+document.querySelector("#facebook").herf = "https://www.facebook.com/KingIMasr";
+document.getElementById("twitter").herf = "https://www.twitter.com/KingIMasr";
+document.getElementById("linkedin").herf = "https://www.linkedin.com/in/king-masr/";
+document.getElementById("telegram").herf = "https://t.me/King_Masr";
