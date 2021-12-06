794fe1437c5c9125e264676bd4be9a5adf3f5d4d
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-07 01:26:57 +0200

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let myTabs = document.querySelectorAll("#login #tabs li");
+let tabs = Array.from(myTabs);
+let mySections = document.querySelectorAll("#login #content section");
+let sections = Array.from(mySections);
+sections.pop();
+tabs.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    tabs.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    sections.forEach((section) => {
+      section.style.display = "none";
+    });
+    document.querySelector(e.currentTarget.dataset.id).style.display = "block";
+  });
+});
