3781f2821e6132ee9bdb1caa0bed1817d332de69
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-06 10:20:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,23 +7,35 @@
 //     let name = $().val();
 //   });
 // });
-window.onload = function () {
+/* window.onload = function () {
   import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
   let auth = getAuth();
   document.getElementById("signup").addEventListener("click", function (e) {
+    e.preventDefault()
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        let user = userCredential.user;
-        console.log(user);
+    .then((userCredential) => {
+      // Signed in
+      let user = userCredential.user;
+      console.log(user);
       })
       .catch((error) => {
         let errorCode = error.code;
         let errorMessage = error.message;
         console.log(errorMessage);
       });
+    });
+}; */
+window.onload = function () {
+  let signupBtn = document.getElementById("signup");
+  signupBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+    let auth = getAuth();
   });
-};
+}
