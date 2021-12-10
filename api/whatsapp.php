a864d53b43a4d5ff4fa42a1015ef8951bf15b31f
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-10 07:23:56 +0200

    Create whatsapp.php

---
+++ api/whatsapp.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]==="get") {
+  $message = "
+  الاسم: ".$_GET["name"]."
+  البريدالإلكتروني: ".$_GET["email"]."
+  الهاتف: ".$_GET["phone"]."
+  الهاتف الأخر: ".$_GET["phone1"]."
+  العنوان: ".$_GET["location"]."
+  معلومات اضافية: ".$_GET["email-message"];
+  $url = "https://api.whatsapp.com/send/?phone=201015791302&text=".$message;
+  header("REFRESH:0;URL:".$message);
+}
