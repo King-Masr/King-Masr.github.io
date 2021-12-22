d90aefe96ccfe7dc05f5c2f8b495f4ee445362c9
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-22 13:10:34 +0200

    Update main.js

--- Projects/Game/main.js
+++ Projects/Game/main.js
@@ -1,12 +1,13 @@
 let database = {
   count: 0,
-  firstPlayer: { name: "", score: 0 },
-  secondPlayer: { name: "", score: 0 },
-  thirdPlayer: { name: "", score: 0 },
+  firstPlayer: { name: "mohammed", score: 10 },
+  secondPlayer: { name: "hosam", score: 7 },
+  thirdPlayer: { name: "body", score: 3 },
   players: {
     0: {
-      score: 50
-    }
+      name: "Aly Ahmed Aly",
+      score: 5,
+    },
   },
 };
 let firstPlayer = document.getElementById("theFirst");
@@ -16,30 +17,47 @@ window.onload = function () {
   firstPlayer.textContent += database.firstPlayer.name;
   secondPlayer.textContent += database.secondPlayer.name;
   thirdPlayer.textContent += database.thirdPlayer.name;
+  check();
 };
 function check() {
-  for (let i = 0; i < database.players.length; i++) {
+  for (let i = 0; i < database.players.constructor.length; i++) {
     if (database.players[i].score > database.firstPlayer.score) {
       database.firstPlayer.name = database.players[i].name;
       database.firstPlayer.score = database.players[i].score;
-      Winner(database.players[i]);
+      Winner(database.players[i], 1);
       continue;
     }
     if (database.players[i].score > database.secondPlayer.score) {
       database.secondPlayer.name = database.players[i].name;
       database.secondPlayer.score = database.players[i].score;
-      Winner(database.players[i]);
+      Winner(database.players[i], 2);
       continue;
     }
     if (database.players[i].score > database.thirdPlayer.score) {
       database.thirdPlayer.name = database.players[i].name;
       database.thirdPlayer.score = database.players[i].score;
-      Winner(database.players[i]);
+      Winner(database.players[i], 3);
       continue;
     }
   }
 }
-function Winner(player = {}) {
-  
+function Winner(player = {}, order = 0) {
+  let alertMessage = document.createElement("section");
+  alertMessage.setAttribute("id", "alert");
+  let mainTitle = document.createElement("h2");
+  mainTitle.textContent = "We have Winner!";
+  alertMessage.appendChild(mainTitle);
+  let container = document.createElement("main");
+  alertMessage.appendChild(container);
+  let giftIcon = document.createElement("i");
+  giftIcon.className = "fas fa-gift fa-10x";
+  container.appendChild(giftIcon);
+  let congratulations = document.createElement("p");
+  congratulations.textContent =
+    "Congratulations " + player.name + " you are number " + order;
+  container.appendChild(congratulations);
+  // view in console
+  console.log(alertMessage);
+  // append to body
+  document.body.appendChild(alertMessage);
 }
-let 
