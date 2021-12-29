b920bca9bb7ee8100f4f7741154dba1d38cdf8db
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-29 08:26:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "/firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app)
 const analytics = getAnalytics(app);
 const {
   initializeAppCheck,
