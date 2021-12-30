21a450fc5dac64f5837f6025fc08de968a84128f
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-30 18:08:58 +0200

    Create lesson-07-09.js

---
+++ assignments/javascript/lesson-07-09.js
@@ -0,0 +1,17 @@
+// assignment 01
+let myName = "Aly Ahmed Aly";
+let myElement = document.getElementById("info");
+myElement.textContent = myName;
+// assignment 02
+let myString = "String"; // Data Type: String
+let myNumber = 0; // Data Type: Number
+let myBoolean = true; // Data Type: Boolean
+let myArray = []; // Data Type: Object
+let myObject = {}; // Data Type: Object
+// assignment 03
+myName = "Aly";
+let myAge = 15;
+let myCountry = "Egypt";
+let myMessage = `Hello My Name Is ${myName}\nAnd Iam ${myAge} Years Old,\nAnd I Live in ${myCountry}`;
+let myMessageElement = document.getElementById("message");
+myMessageElement.innerText = myMessage;
