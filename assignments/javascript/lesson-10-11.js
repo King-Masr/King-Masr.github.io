7cb6e421709115c7241695a86d872c997c3531f0
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-30 18:08:32 +0200

    Create lesson-10-11.js

---
+++ assignments/javascript/lesson-10-11.js
@@ -0,0 +1,27 @@
+// assignment 01
+let myName = "Aly";
+let myAge = 15;
+let myMessage = `Hello ${myName}, Your Age Is ${myAge}`;
+// alert(myMessage);
+// assignment 02
+let myElement = document.createElement("div");
+myElement.textContent = myMessage;
+document.body.appendChild(myElement);
+// assignment 03
+console.log(myMessage);
+console.log("####################");
+console.log(myMessage.slice(0, 10));
+console.log(myMessage.slice(11));
+// assignment 04
+let myFirstNumber = 30;
+let myLastNumber = 20;
+let myCollection = myFirstNumber + myLastNumber;
+let mySubtract = myFirstNumber - myLastNumber;
+let myMultiply = myFirstNumber * myLastNumber;
+let myDividing = myFirstNumber / myLastNumber;
+let myLastOperation = 37 / 7;
+console.log(myCollection);
+console.log(mySubtract);
+console.log(myMultiply);
+console.log(myDividing);
+console.log(myLastOperation);
