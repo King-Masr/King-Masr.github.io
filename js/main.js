7013c738da933c3274d7ce6ca0aff057e0972bf7
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2021-12-30 18:08:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   let type = new TypeIt("#description", {
-    strings: ["I am a Web Developer Front End", "My skills: HTML, CSS, JS, Vue.js, PHP, Json"],
+    strings: [
+      "I am a Web Developer Front End",
+      "My skills: HTML, CSS, JavaScript, Vue.js, JSON, PHP, MySQL, Python",
+    ],
     speed: 300,
     waitUntilVisible: false,
     breakLines: false,
