123ad403e4bd3143da33ed3a166d0d7d53944540
King-Masr <59269647+King-Masr@users.noreply.github.com>
2022-03-28 08:37:03 +0200

    Create lesson-001-009.js

---
+++ assignments/javascript-bootcamp/lesson-001-009.js
@@ -0,0 +1,9 @@
+// assignment 01
+let h1Name = document.createElement("h1");
+h1Name.innerText = "Elzero";
+h1Name.style.color = "blue";
+h1Name.style.fontSize = "80px";
+h1Name.style.fontWeight = "bold";
+h1Name.style.textAlign = "center";
+h1Name.style.fontFamily = "Arial";
+document.body.appendChild(h1Name);
