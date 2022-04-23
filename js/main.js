e2912cf44d6f0bbb634722024394ff4de6b13459
Aly Ahmed Aly <59269647+King-Masr@users.noreply.github.com>
2022-04-23 14:00:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ window.onload = function () {
     loopDelay: 1000,
   }).go();
 };
+function more(box) {
+  let moreP = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
+  ];
+  let myParagraph = document.querySelector(`#features .box:nth-child(${box}) p`);
+  myParagraph.innerText = moreP[box - 1];
+}
+function less(box) {
+  let lessP = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  ];
+  let myParagraph = document.querySelector(`#features .box:nth-child(${box}) p`);
+  myParagraph.innerText = lessP[box - 1];
+}
+for (let i = 0; i < 3; i++) {
+  document.querySelectorAll("#features .box")[i].children[3].onclick = function (e) {
+    if (e.target.href.includes("more")) {
+      e.target.innerText = "Less";
+      setTimeout(function () {
+        e.target.href = e.target.href.replace("more", "less");
+      }, 50);
+    } else {
+      e.target.innerText = "More";
+      setTimeout(function () {
+        e.target.href = e.target.href.replace("less", "more");
+      }, 50);
+    }
+  };
+}
+"aaaaaa".includes()
