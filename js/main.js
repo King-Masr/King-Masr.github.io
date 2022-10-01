17dae15aa9cad565026d3b7ea4cb08fc6854430b
King-Masr <59269647+King-Masr@users.noreply.github.com>
2022-10-01 08:52:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 window.onload = function () {
-  let type = new TypeIt("#description", {
-    strings: [
-      "I am a Full Stack Developer",
-      "I have experience in PHP for 5 years",
-      "My skills: HTML, CSS, JavaScript, PHP, MySQL, Laravel, Ajax, JSON, and more...",
-    ],
-    speed: 400,
-    waitUntilVisible: false,
-    breakLines: false,
-    startDelay: 1000,
-    deleteSpeed: 500,
-    nextStringDelay: 5000,
-    loop: true,
-    loopDelay: 1000,
-  }).go();
+  // let type = new TypeIt("#description", {
+  //   strings: [
+  //     "I am a Full Stack Developer.",
+  //     "I have experience in PHP for 5 years.",
+  //     "My skills: HTML, CSS, JavaScript, PHP, MySQL, Laravel, Ajax, JSON, and more...",
+  //   ],
+  //   speed: 400,
+  //   waitUntilVisible: false,
+  //   breakLines: false,
+  //   startDelay: 1000,
+  //   deleteSpeed: 500,
+  //   nextStringDelay: 5000,
+  //   loop: true,
+  //   loopDelay: 1000,
+  // }).go();
 };
 let upBtn = document.getElementById("up");
 window.onscroll = function () {
