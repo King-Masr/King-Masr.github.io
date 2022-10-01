a1f5d1cc373405fc71d48880761154b41099a373
King-Masr <59269647+King-Masr@users.noreply.github.com>
2022-10-01 07:43:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,13 +15,26 @@ window.onload = function () {
     loopDelay: 1000,
   }).go();
 };
+let upBtn = document.getElementById("up");
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    upBtn.style.display = "flex";
+  } else {
+    upBtn.style.display = "none";
+  }
+};
+upBtn.onclick = function () {
+  window.scrollTo(0, 0);
+};
 function more(box) {
   let moreP = [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, autem maxime. Quae corrupti, reprehenderit praesentium rem deleniti quo ipsa qui nam vero voluptatum facere soluta ex perspiciatis, quos nihil aspernatur!",
   ];
-  let myParagraph = document.querySelector(`#features .box:nth-child(${box}) p`);
+  let myParagraph = document.querySelector(
+    `#features .box:nth-child(${box}) p`
+  );
   myParagraph.innerText = moreP[box - 1];
 }
 function less(box) {
@@ -30,22 +43,24 @@ function less(box) {
     "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   ];
-  let myParagraph = document.querySelector(`#features .box:nth-child(${box}) p`);
+  let myParagraph = document.querySelector(
+    `#features .box:nth-child(${box}) p`
+  );
   myParagraph.innerText = lessP[box - 1];
 }
 for (let i = 0; i < 3; i++) {
-  document.querySelectorAll("#features .box")[i].children[3].onclick = function (e) {
-    if (e.target.href.includes("more")) {
-      e.target.innerText = "Less";
-      setTimeout(function () {
-        e.target.href = e.target.href.replace("more", "less");
-      }, 50);
-    } else {
-      e.target.innerText = "More";
-      setTimeout(function () {
-        e.target.href = e.target.href.replace("less", "more");
-      }, 50);
-    }
-  };
+  document.querySelectorAll("#features .box")[i].children[3].onclick =
+    function (e) {
+      if (e.target.href.includes("more")) {
+        e.target.innerText = "Less";
+        setTimeout(function () {
+          e.target.href = e.target.href.replace("more", "less");
+        }, 50);
+      } else {
+        e.target.innerText = "More";
+        setTimeout(function () {
+          e.target.href = e.target.href.replace("less", "more");
+        }, 50);
+      }
+    };
 }
-"aaaaaa".includes()
